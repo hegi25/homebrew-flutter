47e60c2aa7554a2e464493de9b7cb6ccf8a970bc
ghegedus <hegi25@gmail.com>
2019-03-22 10:10:33 +0100

    Create flutter.rb

---
+++ flutter.rb
@@ -0,0 +1,17 @@
+
+class Flutter < Formula
+  version "1.2.1"
+  sha256 "74ac8397ea29720f116980ea00cf60c34430be1f64489b407f7cf95553babbef"
+  
+  url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v#{version}-stable.zip"
+  name 'flutter'
+  homepage "https://flutter.io/"
+
+  binary "#{staged_path}/flutter/bin/flutter"
+  
+  def install
+    # ENV.deparallelize
+    system "flutter", "doctor"
+  end
+
+end
