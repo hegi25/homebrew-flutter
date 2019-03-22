e3ab2f40c3fe6b69400d5142d14210b17c81a4ce
ghegedus <hegi25@gmail.com>
2019-03-22 09:52:37 +0100

    Update flutter.rb

--- flutter.rb
+++ flutter.rb
@@ -1,7 +1,6 @@
 class Flutter < Formula
   version "1.2.1"
   sha256 "74ac8397ea29720f116980ea00cf60c34430be1f64489b407f7cf95553babbef"
-  
   url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v#{version}-stable.zip"
   name 'flutter'
   homepage "https://flutter.io/"
@@ -17,5 +16,4 @@ class Flutter < Formula
   def install
     system "pod", "setup"
   end
-
 end
