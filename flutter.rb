6cad50976d3e6f95c82d8129f54078ed1ffb9931
ghegedus <hegi25@gmail.com>
2019-03-22 09:37:21 +0100

    Create flutter.rb

---
+++ flutter.rb
@@ -0,0 +1,20 @@
+
+class flutter < Formula
+  version "1.2.1"
+  sha256 "74ac8397ea29720f116980ea00cf60c34430be1f64489b407f7cf95553babbef"
+  
+  url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v#{version}-stable.zip"
+  name 'flutter'
+  homepage "https://flutter.io/"
+
+  depends_on "usbmuxd"
+  depends_on "libimobiledevice"
+  depends_on "ideviceinstaller"
+  depends_on "ios-deploy"
+  depends_on "cocoapods"
+
+  def install
+    system "pod", "setup"
+  end
+
+end
