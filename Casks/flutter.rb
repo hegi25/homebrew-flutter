472b9f7f7168f5b3da2eddd9e4207b6b7e8b558a
ghegedus <hegi25@gmail.com>
2019-03-22 09:02:23 +0100

    Update flutter.rb

--- Casks/flutter.rb
+++ Casks/flutter.rb
@@ -1,7 +1,7 @@
 cask 'flutter' do
-  version "1.0.0"
-  sha256 "0b4cacbe84fd68cc9cfe213a90ddcdba558e45c19be30c6173c07a318be34014"
-
+  version "1.2.1"
+  sha256 "59c0aa1ca7bafdefc879ec66644c5de5543eee59"
+  
   url "https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_v#{version}-stable.zip"
   name 'flutter'
   homepage "https://flutter.io/"
